dcf2b7707334647ffbbfefce2e434fbfa0d8c1cb
baka9131 <baka9131@gmail.com>
2023-08-27 18:46:04 +0900

    feat:government_api

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,45 @@
+import 'package:dio/dio.dart';
 import 'package:dusty_dust/component/category_card.dart';
 import 'package:dusty_dust/component/hourly_card.dart';
 import 'package:dusty_dust/component/main_app_bar.dart';
 import 'package:dusty_dust/component/main_drawer.dart';
 import 'package:dusty_dust/const/colors.dart';
+import 'package:dusty_dust/const/data.dart';
+import 'package:dusty_dust/model/stat_model.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    // ignore: unused_local_variable
+    final response = await Dio().get(
+        'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getCtprvnMesureLIst',
+        queryParameters: {
+          'serviceKey': serviceKey,
+          'returnType': 'json',
+          'numOfRows': 30,
+          'pageNo': 1,
+          'itemCode': 'PM10',
+          'dataGubun': 'HOUR',
+          'searchCondition': 'WEEK',
+        });
+
+    print(response.data['response']['body']['items'].map(
+      (item) => StatModel.fromJson(json: item),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
